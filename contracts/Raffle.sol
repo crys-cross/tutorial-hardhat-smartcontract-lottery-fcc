4d2a7f9466231a32044e578ea09f0932ec26f4c0
CRYS <mariano.christopher123@gmail.com>
2022-06-16 21:38:52 +0800

    Added Raffle contract

---
+++ contracts/Raffle.sol
@@ -0,0 +1,41 @@
+// Raffle blueprint Brainstorming
+//Steps
+//1. Enter the lottery (Paying some amount)
+//2. Pick a random winner(verifiebly random)
+//3. Winner selected every X minutes -> completely automated
+//Needs
+//Chainlink Oracle -? Randomness, Automated execution(Chainlink Keepers)
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+error Raffle__NotEnoughEthEntered();
+
+contract Raffle {
+    /*State Variables*/
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        // require(msg.value > i_entranceFee, "Not enough ETH")
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthEntered();
+        }
+        s_players.push(payable(msg.sender));
+    }
+
+    // function pickRandomWinner() {}
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayers(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,10 @@
-require("dotenv").config()
-
-require("@nomiclabs/hardhat-etherscan")
 require("@nomiclabs/hardhat-waffle")
-require("hardhat-gas-reporter")
-require("solidity-coverage")
+require("@nomiclabs/hardhat-etherscan")
 require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
@@ -14,7 +14,7 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 module.exports = {
     // solidity: "0.8.8",
     solidity: {
-        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+        compilers: [{ version: "0.8.7" }, { version: "0.6.6" }],
     },
     defaultNetwork: "hardhat",
     networks: {
