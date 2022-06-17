6f3481a7ce068b0b818f69ceb23419c80c323b21
CRYS <mariano.christopher123@gmail.com>
2022-06-17 22:35:22 +0800

    Edit Raffle.sol
    aaded chainlink contract and VRFv2

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -10,9 +10,11 @@
 
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle__NotEnoughEthEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /*State Variables*/
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
@@ -20,7 +22,7 @@ contract Raffle {
     /*Events*/
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
@@ -35,8 +37,18 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
-    // function pickRandomWinner() {}
+    function requestRandomWinner() external {
+        // Request a random number
+        // Once we get it, do something  with it
+        // 2 transaction process
+    }
 
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
+
+    /*View/Pure Functions*/
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
