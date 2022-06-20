71f8a54c858dce3b7faa29732e5304ab5c9c7a04
CRYS <mariano.christopher123@gmail.com>
2022-06-20 21:32:57 +0800

    Fix deploy scripts typo errors

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,12 +1,13 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
 
-const VRF_SUB_FUND_AMOUNT = ethers.utils.pareEther("30")
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("30")
 
-module.export = async ({ getNamedAccounts, deployer }) => {
+module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
-    const deployer = await getNamedAccounts()
-    const chainID = network.config.chainId
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
     let vrfCoordinatorV2Address, subscriptionId
 
     if (developmentChains.includes(network.name)) {
@@ -14,30 +15,38 @@ module.export = async ({ getNamedAccounts, deployer }) => {
         vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address
         const transactionResponse = await VRFCoordinatorV2Mock.createSubscription()
         const transactionReceipt = await transactionResponse.wait(1)
-        subscriptionId = transactionReceipt.events(0).args.subId
+        subscriptionId = transactionReceipt.events[0].args.subId
         // Fund the subscription, need LINK token on real network
         await VRFCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
     } else {
-        vrfCoordinatorV2Address = networkConfig[chainID]["vrfCoordinatorV2"]
-        subscriptionId = networkConfig[chainID]["subscriptionId"]
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
     }
 
     const entranceFee = networkConfig[chainId]["entranceFee"]
-    const gasLance = networkConfig[chainId]["gasLance"]
+    const gasLane = networkConfig[chainId]["gasLane"]
     const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
     const interval = networkConfig[chainId]["interval"]
-    const args = [
+    const arguments = [
         vrfCoordinatorV2Address,
         entranceFee,
-        gasLance,
+        gasLane,
         subscriptionId,
         callbackGasLimit,
         interval,
     ]
     const raffle = await deploy("Raffle", {
         from: deployer,
-        args: args,
+        args: arguments,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(raffle.address, arguments)
+    }
+    log("-----------------------------------------")
 }
+
+module.exports.tags = ["all", "raffle"]
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -3,20 +3,30 @@ const { ethers } = require("hardhat")
 const networkConfig = {
     // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
     // Default one is ETH/USD contract on Kovan
-    31337: {
+    dafault: {
         name: "hardhat",
+        ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        subscriptionId: "6727",
+        callbackGasLimit: "500000" /*500,000*/,
+        interval: "30",
+    },
+    31337: {
+        name: "localhost",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
         callbackGasLimit: "500000" /*500,000*/,
         interval: "30",
-    }
+    },
     4: {
         name: "rinkeby",
         ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
         vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-        subscriptionId: "0",
+        subscriptionId: "6727",
         callbackGasLimit: "500000" /*500,000*/,
         interval: "30",
     },
