80bcfe2a90d9959f07da08127820bfdb15fc28ea
CRYS <mariano.christopher123@gmail.com>
2022-06-23 13:54:17 +0800

    Added more final touches to staging test script

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -26,7 +26,7 @@ const networkConfig = {
         vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-        subscriptionId: "6727",
+        subscriptionId: "6727", //for rinkeby
         callbackGasLimit: "500000" /*500,000*/,
         interval: "30",
     },
--- test/staging/raffle.staging.test.js
+++ test/staging/raffle.staging.test.js
@@ -1,6 +1,6 @@
 const { assert, expect } = require("chai")
-const { getNamedAccounts, deployments, ethers, network } = require("hardhat")
-const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
+const { getNamedAccounts, ethers, network } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
 
 developmentChains.includes(network.name)
     ? describe.skip
@@ -16,6 +16,46 @@ developmentChains.includes(network.name)
               it("works with live Chainlink Keepers and Chainlink VRF, we get a random winner", async () => {
                   //only enter the raffle
                   const startingTimeStamp = await raffle.getLatestTimeStamp()
+                  const accounts = await ethers.getSigners()
+
+                  await new Promise(async (resolve, reject) => {
+                      //setup listener before we enter the raffle just incase the blockchain moves really fast
+                      //await raffle.enterRaffle({value: raffleEntranceFee})
+                      raffle.once("WinnerPicked", async () => {
+                          console.log("winnerPicked event fired")
+                          try {
+                              //add asserts here
+                              const recentWinner = await raffle.getRecentWinner()
+                              const raffleState = await raffle.getRaffleState()
+                              const winnerEndingBalance = await accounts[0].getBalance()
+                              const endingTimeStamp = await raffle.getLatestTimeStamp()
+
+                              await expect(raffle.getPlayer()).to.be.reverted
+                              assert.equal(recentWinner.toString(), accounts[0].address)
+                              assert.equal(raffleState, o)
+                              assert.equal(
+                                  winnerEndingBalance.toString(),
+                                  winnerStartingBalance.add(raffleEntranceFee).toString()
+                              )
+                              assert(endingTimeStamp > startingTimeStamp)
+                              resolve()
+                          } catch (error) {
+                              console.log(error)
+                              reject(e)
+                          }
+                      })
+                      //then entering the raffle
+                      await raffle.enterRaffle({ valeu: raffleEntranceFee })
+                      const winnerStartingBalance = await accounts[0].getBalance()
+                      //and this code wont complete until our listener has finished listening
+                  })
               })
           })
       })
+
+//to test on testnet
+//1 Get our subId for Chainlink VRF
+//2 Deploy our contract using the subId
+//3 Register the contract with Chainlink & ist subId
+//4 Register the contract with Chainlink Keepers
+// 5 Run staging test
