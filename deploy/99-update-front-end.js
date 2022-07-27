c61690c8909addefe64431df1539ed8ef94ecee5
CRYS <mariano.christopher123@gmail.com>
2022-07-27 16:49:44 +0800

    added draft front end script
    deploy script to update front end

---
+++ deploy/99-update-front-end.js
@@ -0,0 +1,32 @@
+const fs = require("fs")
+
+const frontEndContractsFile =
+    "../../practice-nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json"
+const frontEndAbiFile = "../../practice-nextjs-smartcontract-lottery-fcc/constants/abi.json"
+
+module.exports = async () => {
+    if (process.env.UPDATE_FRONT_END) {
+        console.log("Updating Front End")
+        updateContractAddresses()
+        updateAbi()
+    }
+}
+const updateContractAddresses = async () => {
+    const raffle = await ethers.getContract("Raffle")
+    const chainId = network.config.chainId.toString()
+    const currentAddresses = JSON.parse(fs.readFileSync(frontEndContractsFile, "utf8"))
+    if (chainId in currentAddresses) {
+        if (!currentAddresses[chainId].includes(raffle.address)) {
+            currentAddresses[chainId].push(raffle.address)
+        }
+    } else {
+        currentAddresses[chainId] = [raffle.address]
+    }
+    fs.writeFileSync(frontEndContractsFile, JSON.stringify(currentAddresses))
+    console.log("Front end written!")
+}
+const updateAbi = async () => {
+    const raffle = await ethers.getContract("Raffle")
+    fs.writeFileSync(frontEndAbiFile, raffle.interface.format(ethers.utils.FormatTypes.json))
+}
+module.exports.tags = ["all", "frontend"]
