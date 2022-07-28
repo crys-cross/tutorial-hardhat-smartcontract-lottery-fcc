e3f8af4cb1c6fea832338aa23c8ea12ef80c5391
CRYS <mariano.christopher123@gmail.com>
2022-07-28 21:39:05 +0800

    added scripts to help frontend

--- deploy/99-update-front-end.ts
+++ deploy/99-update-front-end.ts
@@ -1,7 +1,6 @@
 import fs from "fs"
 import { ethers, network } from "hardhat"
 import { DeployFunction } from "hardhat-deploy/types"
-import { HardhatRuntimeEnvironment } from "hardhat/types"
 
 const frontEndContractsFile =
     "../../practice-nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json"
@@ -30,30 +29,11 @@ const updateContractAddresses = async () => {
 }
 const updateAbi = async () => {
     const raffle = await ethers.getContract("Raffle")
-    fs.writeFileSync(frontEndAbiFile, raffle.interface.format(ethers.utils.FormatTypes.json))
+    fs.writeFileSync(
+        frontEndAbiFile,
+        raffle.interface.format(ethers.utils.FormatTypes.json).toString()
+    )
 }
-
-// const updateUI: DeployFunction = async function (
-//     hre: HardhatRuntimeEnvironment
-//   ) {
-//     const { network, ethers } = hre
-//     const chainId = "31337"
-
-//     if (process.env.UPDATE_FRONT_END) {
-//         console.log("Writing to front end...")
-//         const fundMe = await ethers.getContract("Raffle")
-//         const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractsFile, "utf8"))
-//         if (chainId in contractAddresses) {
-//             if (!contractAddresses[network.config.chainId!].includes(fundMe.address)) {
-//                 contractAddresses[network.config.chainId!].push(fundMe.address)
-//             }
-//         } else {
-//             contractAddresses[network.config.chainId!] = [fundMe.address]
-//         }
-//         fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses))
-//         console.log("Front end written!")
-//     }
-// }
 
 export default updateUI
 updateUI.tags = ["all", "frontend"]
